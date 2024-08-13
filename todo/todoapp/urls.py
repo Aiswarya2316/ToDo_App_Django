5aa62d6e8877403edc3619282e5c01a5a38a6e16
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-08-13 11:29:26 +0530

    todo

--- todo/todoapp/urls.py
+++ todo/todoapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.fun1),
+    path('',views.index),
+    path('insert',views.insert),
+    path('display',views.display),
 ]
--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(req):
+    return render(req,'index.html')
+
+td=[]
+def insert(req):
+    if req.method=="POST":
+        name=req.POST['name']
+        age=req.POST['age']
+        mark=req.POST['mark']
+        ID=req.POST['ID']
+        discription=req.POST['discription']
+        td.append({'nm':name , 'ag':age , 'mk':mark ,'ID':ID , 'dis':discription})
+        print(td)
+        return render(req,'insert.html',{'data':td})
+    else:       
+        return render(req,'insert.html',{'data':td})  
+
+def display(req):
+    return render(req,'display.html',{'data':td})        
